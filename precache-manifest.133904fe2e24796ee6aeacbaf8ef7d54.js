f84653c2d896f3ad61a5489ec13864d0f66dd208
Code Sandbox <hello@codesandbox.io>
2021-05-17 11:26:32 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.133904fe2e24796ee6aeacbaf8ef7d54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45eb0cc2ba8f491ff5582e5ebe0fcc71",
+    "url": "https://dougzybuydesign.github.io/csb-bdpgk/index.html"
+  },
+  {
+    "revision": "8ad7ed6df5186925b87b",
+    "url": "https://dougzybuydesign.github.io/csb-bdpgk/static/css/2.38ca0958.chunk.css"
+  },
+  {
+    "revision": "08fc3d7c0d714f94aa16",
+    "url": "https://dougzybuydesign.github.io/csb-bdpgk/static/css/main.dd503082.chunk.css"
+  },
+  {
+    "revision": "8ad7ed6df5186925b87b",
+    "url": "https://dougzybuydesign.github.io/csb-bdpgk/static/js/2.fc6be164.chunk.js"
+  },
+  {
+    "revision": "0cf806c592412517630465fe68af28fb",
+    "url": "https://dougzybuydesign.github.io/csb-bdpgk/static/js/2.fc6be164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08fc3d7c0d714f94aa16",
+    "url": "https://dougzybuydesign.github.io/csb-bdpgk/static/js/main.ac9d6657.chunk.js"
+  },
+  {
+    "revision": "646f1f7b7f1a3f43d3c1",
+    "url": "https://dougzybuydesign.github.io/csb-bdpgk/static/js/runtime-main.70ea9368.js"
+  }
+]);
